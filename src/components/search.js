34594e1f410ff8329ce911f8a5ba682fb9016c98
Erin Fisher <flybirdie85@gmail.com>
2020-09-30 16:29:35 -0700

    final look through

--- src/components/search.js
+++ src/components/search.js
@@ -5,13 +5,18 @@ import './style.css'
 function Search() {
 
     return (
-        <div>
-        <div class="form-group has-search">
-            <input type="text" class="form-control" placeholder="search">
-            </input>
-        </div>
-        </div>
-    );
+            <div>
+            <div class="form-group has-search">
+                <input type="text" class="form-control" placeholder="search"></input>
+                <button type="submit" className="btn btn-success">Search</button>
+            </div>
+            </div>
+        );
+    
+    
+       
+       
+   
 }
 
 export default Search;
